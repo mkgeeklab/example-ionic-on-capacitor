724ae107145d6dbf240474d0af1d06d37e552322
wf9a5m75 <wf9a5m75@gmail.com>
2020-10-02 23:58:36 -0700

    track the changes on <body>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,15 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+      console.log("Register custom capacitor plugins");
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { GoogleMaps, BaseClass, MapView } from '@test-plugin/capacitor';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  list: any[] = [];
+  counter: number = 0;
+
+  constructor() {
+    // const map: Map = new Plugins.GoogleMaps.Map();
+    // Plugins.GoogleMaps.echo({
+    //   value: 'hello'
+    // }).then((result) => {
+    //   console.log(result);
+    // });
+    // map.someMethod();
+    // Plugins.GoogleMaps.echo({
+    //     value: 'Hello World'
+    //   })
+    //   .then((result: any) => {
+    //     alert(result.value);
+    //   });
+  }
+
+  ngOnInit() {
+    const mapDiv = document.getElementById('hoge');
+    console.log(new MapView(mapDiv));
+  }
+  onListItemClick(item: any) {
+    if (this.list.length === 0) return;
+
+    const idx: number = this.list.indexOf(item);
+    this.list.splice(item, 1);
+  }
+  onButtonClick() {
+    this.list.push({
+      idx: this.counter,
+      label: `count:${++this.counter}`
+    });
+  }
 
 }
